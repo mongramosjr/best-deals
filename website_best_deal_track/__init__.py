c583d8006042f86c2ebd3ee34ebb700eda9b8613
root <mongramosjr@gmail.com>
2016-09-13 12:04:14 +0800

    initial commit for deals in sales order, portal site for best deal and advance feat of deals

---
+++ website_best_deal_track/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    3D2N, Discounts and Coupons
+#    Copyright (C) 2016
+#    Copyright © 2016 Dominador B. Ramos Jr. <mongramosjr@gmail.com>
+#    This file is part of Discounts and Coupons and is released under
+#    the BSD 3-Clause License: https://opensource.org/licenses/BSD-3-Clause
+##############################################################################
+
+import models
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
