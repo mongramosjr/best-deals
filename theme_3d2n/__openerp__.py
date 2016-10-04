459bdaef317e3fd1a917f3068ca0cb59491783fb
root <mongramosjr@gmail.com>
2016-10-04 16:40:30 +0800

    clone bootswatch theme to be sued for 3d2n world website

---
+++ theme_3d2n/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   3D2N Website Theme For Discounts and Coupons
+#   Authors: Dominador B. Ramos Jr. <mongramosjr@gmail.com>
+#   Company: 3D2N World (http://www.3d2nworld.com)
+#
+#   Copyright 2016 3D2N World
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+##############################################################################
+
+{
+    'name': '3D2N Website Theme',
+    'version': '2016.0',
+    'author': 'Dominador B. Ramos Jr.,'
+           '3D2N World',
+    'license': 'Other OSI approved licence',
+    'website': 'https://www.3d2n.deals',
+    'category': 'Website',
+    'summary': 'Website Theme for 3D2N',
+    'description': 'This theme module is exclusively for 3D2N',
+    'depends': ['website_best_deal'],
+    'data': [
+        'views/theme.xml',
+    ],
+    'images': [
+        'static/description/3d2n.png'
+    ],
+    'application': False,
+}
